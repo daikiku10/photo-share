605d47568cd7ce2e595ba5f3077fa671e17d05fd
daikiku10 <soccerdk19.910@gmail.com>
2024-04-08 23:02:58 +0900

    DB初期時にテーブル作成SQL

---
+++ cdk/assets/common/rds/00-appTemplate-CreateTable.sql
@@ -0,0 +1,10 @@
+SET NAMES 'utf8mb4';
+DROP DATABASE IF EXISTS appTemplateDataBase;
+CREATE DATABASE IF NOT EXISTS appTemplateDataBase CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE appTemplateDataBase;
+DROP TABLE IF EXISTS appTemplateTable;
+CREATE TABLE IF NOT EXISTS appTemplateTable(
+        ID char(26) NOT NULL,
+        Name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        PRIMARY KEY (ID)
+    );
