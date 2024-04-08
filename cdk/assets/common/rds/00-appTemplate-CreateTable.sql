fd3191bb394880df51a4c2788b55b1b6eabffb95
daikiku10 <soccerdk19.910@gmail.com>
2024-04-09 08:05:37 +0900

    DB初期化バッチの作成

--- cdk/assets/common/rds/00-appTemplate-CreateTable.sql
+++ cdk/assets/common/rds/00-appTemplate-CreateTable.sql
@@ -5,6 +5,6 @@ USE appTemplateDataBase;
 DROP TABLE IF EXISTS appTemplateTable;
 CREATE TABLE IF NOT EXISTS appTemplateTable(
         ID char(26) NOT NULL,
-        Name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        Name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
         PRIMARY KEY (ID)
     );
---
+++ devtool/backendtest/initdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# MySQL初期化バッチ
+# 2階層下までのファイルを探して、SQLファイルだった場合はMysqlへ渡して実行する
+
+echo "$0: initializing database"
+find /docker-entrypoint-initdb.d -mindepth 2 -type f | sort | while read f; do
+  case "$f" in
+      *.sql)    echo "$0: running $f"; MYSQL_PWD=my-secret-pw mysql -uroot mydb < "$f" 2>&1;;
+      *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}" 2>&1;;
+      *)        echo "$0: ignoring $f" ;;
+  esac
+done
+echo "$0: initialized"
+echo
