3a03d12fa7ec8ffa479ef70e27a1288710eb3408
daikiku10 <soccerdk19.910@gmail.com>
2024-04-18 07:42:44 +0900

    ヘッダーからユーザー情報を取得する処理

--- back/sharelib/user/user.go
+++ back/sharelib/user/user.go
@@ -1,9 +1,55 @@
 package user
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"photo-share/back/sharelib/domain/logging"
+)
+
 // ID ユーザーID
 type ID string
 
 // go:generate accessor -type=User
 type User struct {
 	id ID
+}
+
+func New(id ID) *User {
+	return &User{
+		id: id,
+	}
+}
+
+// NewWithBase64Json Base64にエンコードされたユーザー情報をデコードする
+func NewWithBase64Json(encoded string) (*User, error) {
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		logging.Error("ユーザー情報のBase64 Decodeに失敗しました。")
+		return nil, err
+	}
+
+	// jsonからUser構造体への変換
+	user, err := unmarshalJSON(decoded)
+	if err != nil {
+		logging.Error("ユーザー情報のJson Parseに失敗しました。")
+		return nil, err
+	}
+
+	return user, nil
+}
+
+type userInformation struct {
+	Id ID `json:"Id"`
+}
+
+// unmarshalJSON JSONをUser構造体に変換する
+func unmarshalJSON(data []byte) (*User, error) {
+	var userInfo userInformation
+	if err := json.Unmarshal(data, &userInfo); err != nil {
+		return nil, err
+	}
+	user := New(
+		userInfo.Id,
+	)
+	return user, nil
 }
--- back/usecase/handler/postPhoto.go
+++ back/usecase/handler/postPhoto.go
@@ -1,7 +1,22 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"photo-share/back/sharelib/user"
+
+	"github.com/gin-gonic/gin"
+)
 
 // PostPhoto 投稿写真登録API
 // (POST /photo/create)
-func (se *Server) PostPhoto(ctx *gin.Context) {}
+func (s *Server) PostPhoto(ctx *gin.Context) {
+	// TODO: ヘッダーにユーザー情報を受け取れるようにする
+	user, err := user.NewWithBase64Json("")
+	if err != nil {
+		// TODO: エラー内容の返却
+		ctx.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	fmt.Println(user)
+}
