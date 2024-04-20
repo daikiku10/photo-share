6caea2b64ccd66fdc017e039260dcfdaaa79b625
daikiku10 <soccerdk19.910@gmail.com>
2024-04-21 07:30:04 +0900

    requestBodyの取得処理

--- back/usecase/handler/postPhoto.go
+++ back/usecase/handler/postPhoto.go
@@ -1,10 +1,9 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
-	ErrorCode "photo-share/back/errorcode"
-	DomainError "photo-share/back/sharelib/domain/error"
-	"photo-share/back/sharelib/user"
+	"photo-share/back/infrastructure/restexe/apidef/server"
 	"photo-share/back/usecase/converter"
 
 	"github.com/gin-gonic/gin"
@@ -14,21 +13,29 @@ import (
 // (POST /photo/create)
 func (s *Server) PostPhoto(ctx *gin.Context) {
 	// TODO: ヘッダーにユーザー情報を受け取れるようにする
-	user, err := user.NewWithBase64Json("")
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, converter.ToErrorItem(DomainError.NewErrorWithInner(ErrorCode.NotAuthorized, err)))
-		return
-	}
+	user := ""
+	// user, errHeader := user.NewWithBase64Json("")
+	// if errHeader != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, converter.ToErrorItem(DomainError.NewErrorWithInner(ErrorCode.NotAuthorized, errHeader)))
+	// 	return
+	// }
 
 	// トランザクションの取得
 	transactor := s.resourceProvider.GetCurrentTransactor(ctx)
 	// リポジトリの取得
-	repo, err := s.resourceProvider.NewRepository(transactor)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, converter.ToErrorItem(err))
+	repo, errRepo := s.resourceProvider.NewRepository(transactor)
+	if errRepo != nil {
+		ctx.JSON(http.StatusInternalServerError, converter.ToErrorItem(errRepo))
 		return
 	}
 
 	// requestBodyを取得
-
+	var requestBody server.PostPhotoRequest
+	errBind := ctx.ShouldBindJSON(&requestBody)
+	if errBind != nil {
+		ctx.JSON(http.StatusInternalServerError, converter.ToErrorItem(errBind))
+		return
+	}
+	fmt.Println(user, repo)
+	fmt.Print(requestBody)
 }
