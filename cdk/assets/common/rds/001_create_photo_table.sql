c9a9d9d93a3c384b368a24dc9447b11917bf19a2
daikiku10 <soccerdk19.910@gmail.com>
2024-04-23 21:45:10 +0900

    日時カラムの追加

--- cdk/assets/common/rds/001_create_photo_table.sql
+++ cdk/assets/common/rds/001_create_photo_table.sql
@@ -5,5 +5,8 @@ CREATE DATABASE IF NOT EXISTS photo_share CHARACTER SET utf8mb4 COLLATE utf8mb4_
 CREATE TABLE `photo_share`.`photos` (
     `id` char(26) not null COMMENT '主キー',
     `title` varchar(255) not null COMMENT '写真タイトル',
+    `created_at` datetime not null default current_timestamp COMMENT '作成日時',
+    `updated_at` datetime not null default current_timestamp on update current_timestamp COMMENT '更新日時',
     PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='写真'
+    ;
