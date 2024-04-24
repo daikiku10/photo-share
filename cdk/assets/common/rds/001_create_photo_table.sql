82c39edcc693b4d31834bc03efde1d50c4361c75
daikiku10 <soccerdk19.910@gmail.com>
2024-04-24 22:02:17 +0900

    photosテーブルにカラム追加

--- cdk/assets/common/rds/001_create_photo_table.sql
+++ cdk/assets/common/rds/001_create_photo_table.sql
@@ -5,6 +5,8 @@ CREATE DATABASE IF NOT EXISTS photo_share CHARACTER SET utf8mb4 COLLATE utf8mb4_
 CREATE TABLE `photo_share`.`photos` (
     `id` char(26) not null COMMENT '主キー',
     `title` varchar(255) not null COMMENT '写真タイトル',
+    `created_by` char(26) not null COMMENT '作成者',
+    `updated_by` char(26) not null COMMENT '更新者',
     `created_at` datetime not null default current_timestamp COMMENT '作成日時',
     `updated_at` datetime not null default current_timestamp on update current_timestamp COMMENT '更新日時',
     PRIMARY KEY(`id`)
