c4966d94e0d47eae83d306ad73df4c101a63b531
daikiku10 <soccerdk19.910@gmail.com>
2024-04-26 23:17:26 +0900

    Photosテーブルの作成SQL

--- cdk/assets/common/rds/001_create_photo_table.sql
+++ cdk/assets/common/rds/001_create_photo_table.sql
@@ -4,11 +4,12 @@ CREATE DATABASE IF NOT EXISTS photo_share CHARACTER SET utf8mb4 COLLATE utf8mb4_
 
 CREATE TABLE `photo_share`.`photos` (
     `id` char(26) not null COMMENT '主キー',
-    `title` varchar(255) not null COMMENT '写真タイトル',
-    `created_by` char(26) not null COMMENT '作成者',
-    `updated_by` char(26) not null COMMENT '更新者',
     `created_at` datetime not null default current_timestamp COMMENT '作成日時',
-    `updated_at` datetime not null default current_timestamp on update current_timestamp COMMENT '更新日時',
-    PRIMARY KEY(`id`)
+    `title` varchar(255) not null COMMENT '写真タイトル',
+    `description` varchar(255) not null COMMENT '説明',
+    `imageUrl` varchar(255) not null COMMENT '写真URL',
+    `userId` char(26) not null COMMENT '投稿者ID',
+    `categoryId` char(26) not null COMMENT 'カテゴリID',
+    CONSTRAINT `Photo_pkey` PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='写真'
     ;
