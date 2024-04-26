63a5446fde8ccccf670e1abeda8437c2f43f8ffa
daikiku10 <soccerdk19.910@gmail.com>
2024-04-27 07:39:51 +0900

    likeとcommentテーブル作成SQL

--- cdk/assets/common/rds/001_create_photo_table.sql
+++ cdk/assets/common/rds/001_create_photo_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE `photo_share`.`photos` (
     `title` varchar(255) not null COMMENT '写真タイトル',
     `description` varchar(255) not null COMMENT '説明',
     `imageUrl` varchar(255) not null COMMENT '写真URL',
-    `userId` char(26) not null COMMENT '投稿者ID',
+    `authorId` char(26) not null COMMENT '投稿者ID',
     `categoryId` char(26) not null COMMENT 'カテゴリID',
     CONSTRAINT `Photo_pkey` PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='写真'
@@ -24,5 +24,26 @@ CREATE TABLE `photo_share`.`category` (
     `description` varchar(255) not null COMMENT '説明',
     `imageUrl` varchar(255) not null COMMENT '写真URL',
     CONSTRAINT `Category_pkey` PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='カテゴリ'
+    ;
+
+-- CreateLike
+CREATE TABLE `photo_share`.`like` (
+    `id` char(26) not null COMMENT '主キー',
+    `created_at` datetime not null default current_timestamp COMMENT '作成日時',
+    `userId` char(26) not null COMMENT 'ユーザーID',
+    `photoId` char(26) not null COMMENT '写真ID',
+    CONSTRAINT `Like_pkey` PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='カテゴリ'
+    ;
+
+-- CreateComment
+CREATE TABLE `photo_share`.`comment` (
+    `id` char(26) not null COMMENT '主キー',
+    `created_at` datetime not null default current_timestamp COMMENT '作成日時',
+    `authorId` char(26) not null COMMENT '投稿者ID',
+    `photoId` char(26) not null COMMENT '写真ID',
+    `comment` varchar(255) not null COMMENT 'コメント',
+    CONSTRAINT `Like_pkey` PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='カテゴリ'
     ;
