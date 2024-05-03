4563c7720d2db0718f022ccb8cd5ca42a461eed4
daikiku10 <soccerdk19.910@gmail.com>
2024-05-03 12:57:10 +0900

    Mainコンポーネントの作成

--- front/src/app/(site)/layout.tsx
+++ front/src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@/components/Layout/Container";
 import { Footer } from "@/components/Layout/Footer";
 import { Header } from "@/components/Layout/Header";
+import { Main } from "@/components/Layout/Main";
 import { Root } from "@/components/Layout/Root";
 
 type Props = {
@@ -11,7 +12,9 @@ export default function SiteLayout({ children }: Props) {
   return (
     <Root>
       <Header />
-      <Container>{children}</Container>
+      <Container>
+        <Main>{children}</Main>
+      </Container>
       <Footer />
     </Root>
   );
