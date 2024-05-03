75950313adcd00b67c85f364544325f58ee41c6c
daikiku10 <soccerdk19.910@gmail.com>
2024-05-03 11:45:35 +0900

    Avatarコンポーネントの作成

---
+++ front/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import styles from "./style.module.css";
+type Props = {
+  size?: "small" | "medium" | "large";
+  avatarImageUrl?: string | null;
+};
+
+export function Avatar({ size = "medium", avatarImageUrl }: Props) {
+  return (
+    <span
+      className={clsx(styles.avatar, styles[size])}
+      {...(avatarImageUrl && {
+        style: { backgroundImage: `url(${avatarImageUrl})` },
+      })}
+    />
+  );
+}
