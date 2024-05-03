9563eedb2bdf732f077fff30aa95d3155685385f
daikiku10 <soccerdk19.910@gmail.com>
2024-05-03 13:57:17 +0900

    サイドバーの作成

--- front/src/app/(site)/layout.tsx
+++ front/src/app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navigation } from "@/app/(site)/_components/Navigation";
 import { Container } from "@/components/Layout/Container";
 import { Footer } from "@/components/Layout/Footer";
 import { Header } from "@/components/Layout/Header";
@@ -13,6 +14,7 @@ export default function SiteLayout({ children }: Props) {
     <Root>
       <Header />
       <Container>
+        <Navigation />
         <Main>{children}</Main>
       </Container>
       <Footer />
