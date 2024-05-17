df19b2016c880ef30ccdeb9f6f4fc707e0b4b2b1
daikiku10 <soccerdk19.910@gmail.com>
2024-05-18 08:19:43 +0900

    postPhotosにCORS対応を入れない

--- bff/src/app/api/photos/route.ts
+++ bff/src/app/api/photos/route.ts
@@ -1,11 +1,5 @@
 import { postPhotos } from "@/app/services/postPhotos";
-import { NextRequest, NextResponse } from "next/server";
-
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:3000",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // OPTONSを追加
-  "Access-Control-Allow-Headers": "Content-Type", // 追加
-};
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   // TODO: セッション情報を取得する
@@ -22,18 +16,9 @@ export async function POST(req: NextRequest) {
       { id },
       {
         status: 201,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
       }
     );
   } catch (err) {
     return Response.json({ message: "Internal Server Error" }, { status: 500 });
   }
-}
-
-export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
 }
