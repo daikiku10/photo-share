84fe7351078bcb04965b9ff6de3a622a374884e8
daikiku10 <soccerdk19.910@gmail.com>
2024-05-19 14:34:57 +0900

    BFFの環境変数設定

--- bff/src/middleware.ts
+++ bff/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [process.env.PHOTO_SHARE_HOST];
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
